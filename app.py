826f354634725db447ef60e9a845d2e6836a9dea
maheshmarar <139688887+maheshmarar@users.noreply.github.com>
2023-12-05 16:06:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,18 +47,7 @@ st.markdown("""
 <h1 style='color: #fba700; text-align: center;'>Exploratory Data Analysis Wizard</h1>
 """, unsafe_allow_html=True)
 
-st.write('<p style="font-size:160%">You will be able to perform below actions:</p>', unsafe_allow_html=True)
 
-st.write('<p style="font-size:100%">&nbsp 1. See the whole dataset</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 2. Get column names, data types info</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 3. Get the count and percentage of NA values</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 4. Get descriptive analysis </p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 5. Check inbalance or distribution of target variable:</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 6. See distribution of numerical columns</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 7. See count plot of categorical columns</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 8. Get outlier analysis with box plots</p>', unsafe_allow_html=True)
-st.write('<p style="font-size:100%">&nbsp 9. Obtain info of target value variance with categorical columns</p>', unsafe_allow_html=True)
-#st.image('header2.png', use_column_width = True)
 
 functions.space()
 st.write('<p style="font-size:130%">Import Dataset</p>', unsafe_allow_html=True)
